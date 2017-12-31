7119ce842a36fcdaf708fd55607b2dd95fc1ac8a
wqhuser <wqhuser@163.com>
2017-12-31 15:29:36 +0800

    add logs

--- Study/src/chapter5mapper/test/Test171231.java
+++ Study/src/chapter5mapper/test/Test171231.java
@@ -5,6 +5,8 @@ import java.io.FileNotFoundException;
 
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 import chapter5mapper.mapper.EmployeeMapper;
 import chapter5mapper.vo.Employee;
@@ -13,24 +15,26 @@ import chapter5mapper.vo.MaleEmployee;
 
 public class Test171231 {
 	public static void main(String[] args) throws FileNotFoundException {
+		PropertyConfigurator.configure("D:\\workspace\\Chapter3\\bin\\com\\learn\\ssm\\chapter3\\main\\log4j.properties");
+		Logger logger = Logger.getLogger(Test171231.class);
 		String config = "mybatis-config.xml";
 		FileInputStream resource = new FileInputStream("E:\\git\\Test\\Study\\src\\chapter5mapper\\test\\" + config);
 		SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(resource);
 		EmployeeMapper mapper = factory.openSession().getMapper(EmployeeMapper.class);
 		Employee me = mapper.getEmployee(123);
-		System.out.println(me.getRealName());
-		System.out.println(me.getSex());
-		System.out.println(me.getWorkCard().getDepartment());
-		System.out.println(me.getEmployeeTaskList().get(0).getTaskName());
-		System.out.println(me.getBirthday());
-		System.out.println(((MaleEmployee)me).getMaleHealthForm().getProstate());
-		System.out.println();
+		logger.info(me.getRealName());
+		logger.info(me.getSex());
+		logger.info(me.getWorkCard().getDepartment());
+		logger.info(me.getEmployeeTaskList().get(0).getTaskName());
+		logger.info(me.getBirthday());
+		logger.info(((MaleEmployee)me).getMaleHealthForm().getProstate());
+		logger.info("\n");
 		Employee test = mapper.getEmployee(456);
-		System.out.println(test.getRealName());
-		System.out.println(test.getBirthday());
-		System.out.println(test.getSex());
-		System.out.println(((FemaleEmployee)test).getFemaleHealthForm().getUterus());
-		System.out.println(test.getWorkCard().getDepartment());
-		System.out.println(test.getEmployeeTaskList().get(0).getTaskName());
+		logger.info(test.getRealName());
+		logger.info(test.getBirthday());
+		logger.info(test.getSex());
+		logger.info(((FemaleEmployee)test).getFemaleHealthForm().getUterus());
+		logger.info(test.getWorkCard().getDepartment());
+		logger.info(test.getEmployeeTaskList().get(0).getTaskName());
 	}
 }
